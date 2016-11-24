1fe51762fed1c3dfe8140c9eb68579a2357e0416
Renaud Amsellem <renaud.jetpulp@gmail.com>
2016-11-24 16:47:15 +0100

    Indexation on order from synchronous to asynchronous to speed up order confirmation

---
+++ app/code/community/Demac/MultiLocationInventory/Model/Resource/Order/Asynchronous/Index/Collection.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Class Demac_MultiLocationInventory_Model_Resource_Order_Stock_Source_Collection
+ */
+class Demac_MultiLocationInventory_Model_Resource_Order_Asynchronous_Index_Collection extends Mage_Core_Model_Resource_Db_Collection_Abstract
+{
+    /**
+     * Init collection
+     */
+    protected function _construct()
+    {
+        $this->_init('demac_multilocationinventory/order_asynchronous_index');
+    }
+}
